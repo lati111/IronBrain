1844fe49f461882a4e01a9db4a694ee99e8a3b06
lati111 <jvzelst111@gmail.com>
2023-04-19 22:48:38 +0200

    Added database migrations

---
+++ database/migrations/2023_04_19_203602_create_project_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->text('description');
+            $table->string('thumbnail');
+            $table->string('route')->nullable();
+            $table->integer('nav_order')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project');
+    }
+};
