703f13bc2a87968630559b6d8d6555d3d3123010
lati111 <jvzelst111@gmail.com>
2023-04-20 18:12:07 +0200

    Made project cards dynamic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Service\TimeService;
+use DateTime;
+
 class HomeController
 {
     public function show()
     {
-        return view('home');
+        $projects = [];
+
+        foreach (Project::all() as $project) {
+            $timeAgo = TimeService::time_elapsed_string($project->updated_at);
+            $array = [
+                "name" => $project->name,
+                "description" => $project->description,
+                "thumbnail" => $project->thumbnail,
+                "route" => $project->route,
+                "timeAgo" => $timeAgo,
+            ];
+
+            $projects[] = $array;
+        }
+
+        return view('home', [
+            "projects" => $projects,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,19 +17,23 @@
         </div>
     </section>
 
-    <div class="album pb-5">
-        <div class="container flex justify-center">
-            <div class="w-96">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <small class="text-body-secondary" title="Last Updated">9 mins</small>
-                    </div>
+    {{--| project cards |--}}
+    @foreach ($projects as $project)
+        <div class="project pb-5">
+            <div class="container flex justify-center">
+                <div class="w-96">
+                    <div class="card shadow-sm">
+                        <img src="{{asset("img/project/sprites/".$project["thumbnail"])}}" alt="{{$project["name"]}}">
+                        <div class="card-body">
+                            <h4 class="title text-center">{{$project["name"]}}</h4>
+                            <p class="card-text text-center">{{$project["description"]}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small class="text-body-secondary" title="Last Updated">{{$project["timeAgo"]}}</small>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endforeach
 @stop
