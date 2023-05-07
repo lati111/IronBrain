b26972a1ea1ae7085bbf896c86f3225747605fc7
lati111 <jvzelst111@gmail.com>
2023-05-07 16:42:50 +0200

    Converted login to form layout

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -1,51 +1,31 @@
-@extends('layouts.master')
+@extends('layouts.form.single_form')
 
-@section('htmlTitle', 'Add Submenu')
+@section('htmlTitle', 'Log In')
+@section('form_title', 'Log In')
+@section('submit_string', 'Log In')
 
-@section('content')
+@section('form_content')
+    {{--| email field |--}}
+    @component('components.form.input_wrapper')
+        @slot('label_text')Email @endslot
+        @slot('input_html')
+            <input type="email" name="email" class="largeInput underlined" placeholder="Email Adress"
+                @if(old('email') !== null) value="{{old('email')}}" @endif
+                required
+            />
+        @endslot
+    @endcomponent
 
-{{--| pagination form |--}}
-<div class="flex justify-center">
-    <div>
-        {{--| title |--}}
-        <div class="flex flex-row justify-center mb-3">
-            <h3 class="title">
-                Log In
-            </h3>
-        </div>
-
-        {{--| form |--}}
-        <div class="flex flex-row justify-center mb-3">
-            <form id="form" action="{{route('auth.login.attempt')}}" method="POST" enctype="multipart/form-data">
-                @csrf
-
-                <div class="flex flex-col justify-center gap-4">
-                    {{--| email field |--}}
-                    <input type="email" name="email" class="largeInput underlined" placeholder="Email Adress"
-                        @if(old('email') !== null) value="{{old('email')}}" @endif
-                        required
-                    />
-
-                    {{--| password field |--}}
-                    <input type="password" name="password" class="largeInput underlined" placeholder="Password"
-                        @if(old('password') !== null) value="{{old('password')}}" @endif
-                        required
-                    />
-                </div>
-
-                {{--| submitter |--}}
-                <div class="flex flex-col mt-3">
-                    <input type="submit" class="interactive" value="Create Account">
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
+    {{--| password field |--}}
+    @component('components.form.input_wrapper')
+        @slot('label_text')Password @endslot
+        @slot('input_html')
+            <input type="password" name="password" class="largeInput underlined" placeholder="Password"
+                @if(old('password') !== null) value="{{old('password')}}" @endif
+                required
+            />
+        @endslot
+    @endcomponent
 @stop
 
-@section('script')
-@vite([
-    'resources/ts/main.ts',
-])
-@stop
+@section('submit_route', route('config.permission.save'))
---
+++ resources/views/components/form/input_wrapper.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col">
+    <label class="text-sm ml-3 form_label">{{$label_text}}</label>
+    {{$input_html}}
+</div>
