32a8efbfb1e79ab5ea503b373c9c8b1d2303873c
lati111 <jvzelst111@gmail.com>
2023-05-07 15:35:02 +0200

    Added route guards

--- routes/config.php
+++ routes/config.php
@@ -17,62 +17,136 @@ Route::prefix('/config')->group(function() {
         Route::get('/', [ProjectController::class, 'overview'])
             ->middleware('permission:config.project.view')
             ->name("config.projects.overview");
-        Route::get('/new', [ProjectController::class, 'new'])->name("config.projects.new");
-        Route::get('/modify/{id}', [ProjectController::class, 'modify'])->name("config.projects.modify");
-        Route::post('/save', [ProjectController::class, 'save'])->name("config.projects.save");
-        Route::post('/delete/{id}', [ProjectController::class, 'delete'])->name("config.projects.delete");
+
+        Route::get('/new', [ProjectController::class, 'new'])
+            ->middleware('permission:config.project.edit')
+            ->name("config.projects.new");
+
+        Route::get('/modify/{id}', [ProjectController::class, 'modify'])
+            ->middleware('permission:config.project.edit')
+            ->name("config.projects.modify");
+
+        Route::post('/save', [ProjectController::class, 'save'])
+            ->middleware('permission:config.project.edit')
+            ->name("config.projects.save");
+
+        Route::post('/delete/{id}', [ProjectController::class, 'delete'])
+            ->middleware('permission:config.project.edit')
+            ->name("config.projects.delete");
+
 
         //| submenu
         Route::prefix('/{project_id}/submenu')->group(function() {
-            Route::get('/new', [SubmenuController::class, 'new'])->name("config.projects.submenu.new");
-            Route::get('/modify/{id}', [SubmenuController::class, 'modify'])->name("config.projects.submenu.modify");
-            Route::post('/save', [SubmenuController::class, 'save'])->name("config.projects.submenu.save");
-            Route::post('/delete/{id}', [SubmenuController::class, 'delete'])->name("config.projects.submenu.delete");
+            Route::get('/new', [SubmenuController::class, 'new'])
+                ->middleware('permission:config.project.edit')
+                ->name("config.projects.submenu.new");
 
-            Route::get('/overview/data', [SubmenuDatatable::class, 'overviewData'])->name("config.projects.submenu.overview.datatable");
+            Route::get('/modify/{id}', [SubmenuController::class, 'modify'])
+                ->middleware('permission:config.project.edit')
+                ->name("config.projects.submenu.modify");
+
+            Route::post('/save', [SubmenuController::class, 'save'])
+                ->middleware('permission:config.project.edit')
+                ->name("config.projects.submenu.save");
+
+            Route::post('/delete/{id}', [SubmenuController::class, 'delete'])
+                ->middleware('permission:config.project.edit')
+                ->name("config.projects.submenu.delete");
+
+            Route::get('/overview/data', [SubmenuDatatable::class, 'overviewData'])
+                ->middleware('permission:config.project.edit')
+                ->name("config.projects.submenu.overview.datatable");
         });
     });
+
 
     //| user control
     Route::prefix('/user')->group(function() {
         // pages
-        Route::get('/', [UserController::class, 'overview'])->name("config.user.overview");
-        Route::post('/delete/{uuid}', [UserController::class, 'delete'])->name("config.user.delete");
+        Route::get('/', [UserController::class, 'overview'])
+            ->middleware('permission:config.user.view')
+            ->name("config.user.overview");
+
+        Route::post('/delete/{uuid}', [UserController::class, 'delete'])
+            ->middleware('permission:config.user.edit')
+            ->name("config.user.delete");
 
         // datatables
-        Route::get('/overview/data', [UserDatatable::class, 'overviewData'])->name("config.user.overview.datatable");
+        Route::get('/overview/data', [UserDatatable::class, 'overviewData'])
+            ->middleware('permission:config.user.view')
+            ->name("config.user.overview.datatable");
 
         // ajax calls
-        Route::post('/{uuid}/role/set', [UserController::class, 'setPermission'])->name("config.user.role.set");
+        Route::post('/{uuid}/role/set', [UserController::class, 'setPermission'])
+            ->middleware('permission:config.user.edit')
+            ->name("config.user.role.set");
     });
+
 
     //| role
     Route::prefix('/role')->group(function() {
         // pages
-        Route::get('/', [RoleController::class, 'overview'])->name("config.role.overview");
-        Route::get('/new', [RoleController::class, 'new'])->name("config.role.new");
-        Route::get('/modify/{id}', [RoleController::class, 'modify'])->name("config.role.modify");
-        Route::post('/save', [RoleController::class, 'save'])->name("config.role.save");
-        Route::post('/delete/{id}', [RoleController::class, 'delete'])->name("config.role.delete");
+        Route::get('/', [RoleController::class, 'overview'])
+            ->middleware('permission:config.role.view')
+            ->name("config.role.overview");
+
+        Route::get('/new', [RoleController::class, 'new'])
+            ->middleware('permission:config.role.edit')
+            ->name("config.role.new");
+
+        Route::get('/modify/{id}', [RoleController::class, 'modify'])
+            ->middleware('permission:config.role.edit')
+            ->name("config.role.modify");
+
+        Route::post('/save', [RoleController::class, 'save'])
+            ->middleware('permission:config.role.edit')
+            ->name("config.role.save");
+
+        Route::post('/delete/{id}', [RoleController::class, 'delete'])
+            ->middleware('permission:config.role.edit')
+            ->name("config.role.delete");
 
         // datatables
-        Route::get('/overview/data', [RoleDatatable::class, 'overviewData'])->name("config.role.overview.datatable");
-        Route::get('/{id}/permissions/data', [PermissionDatatable::class, 'listToggleableData'])->name("config.role.permission.datatable");
+        Route::get('/overview/data', [RoleDatatable::class, 'overviewData'])
+            ->middleware('permission:config.role.view')
+            ->name("config.role.overview.datatable");
+
+        Route::get('/{id}/permissions/data', [PermissionDatatable::class, 'listToggleableData'])
+            ->middleware('permission:config.role.edit')
+            ->name("config.role.permission.datatable");
 
         // ajax calls
-        Route::post('/{role_id}/permission/{permission_id}/toggle', [RoleController::class, 'togglePermission'])->name("config.role.permission.toggle");
+        Route::post('/{role_id}/permission/{permission_id}/toggle', [RoleController::class, 'togglePermission'])
+            ->middleware('permission:config.role.edit')
+            ->name("config.role.permission.toggle");
     });
 
     //| permission
     Route::prefix('/permission')->group(function() {
         // pages
-        Route::get('/', [PermissionController::class, 'overview'])->name("config.permission.overview");
-        Route::get('/new', [PermissionController::class, 'new'])->name("config.permission.new");
-        Route::get('/modify/{id}', [PermissionController::class, 'modify'])->name("config.permission.modify");
-        Route::post('/save', [PermissionController::class, 'save'])->name("config.permission.save");
-        Route::post('/delete/{id}', [PermissionController::class, 'delete'])->name("config.permission.delete");
+        Route::get('/', [PermissionController::class, 'overview'])
+            ->middleware('permission:config.permission.view')
+            ->name("config.permission.overview");
+
+        Route::get('/new', [PermissionController::class, 'new'])
+            ->middleware('permission:config.permission.edit')
+            ->name("config.permission.new");
+
+        Route::get('/modify/{id}', [PermissionController::class, 'modify'])
+            ->middleware('permission:config.permission.edit')
+            ->name("config.permission.modify");
+
+        Route::post('/save', [PermissionController::class, 'save'])
+            ->middleware('permission:config.permission.edit')
+            ->name("config.permission.save");
+
+        Route::post('/delete/{id}', [PermissionController::class, 'delete'])
+            ->middleware('permission:config.permission.edit')
+            ->name("config.permission.delete");
 
         // datatables
-        Route::get('/overview/data', [PermissionDatatable::class, 'overviewData'])->name("config.permission.overview.datatable");
+        Route::get('/overview/data', [PermissionDatatable::class, 'overviewData'])
+            ->middleware('permission:config.permission.view')
+            ->name("config.permission.overview.datatable");
     });
 });
