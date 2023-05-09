d8d711282d07b929b3acc8d9fa11c33c39f71151
lati111 <jvzelst111@gmail.com>
2023-05-09 20:14:21 +0200

    Added permission filtering to projects

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Config\Project;
 use App\Service\TimeService;
 
@@ -9,11 +10,15 @@ class HomeController extends Controller
 {
     public function show()
     {
+        $role_id = null;
+        $user = Auth::user();
+        if ($user !== null) {
+            $role_id = $user->role_id;
+        }
+
         $projects = [];
 
-        $projectCollection = Project::where('in_overview', true)
-            ->orderBy('updated_at', 'desc')
-            ->get();
+        $projectCollection = $this->getProjects($role_id);
 
         foreach ($projectCollection as $project) {
             $timeAgo = TimeService::time_elapsed_string($project->updated_at);
@@ -31,5 +36,27 @@ class HomeController extends Controller
         return view('home', array_merge($this->getBaseVariables(), [
             "projects" => $projects,
         ]));
+    }
+
+    private function getProjects(?int $role_id) {
+        return Project::select('nav__project.*')
+            ->leftJoin(
+                'auth__permission',
+                'nav__project.permission_id',
+                '=',
+                'auth__permission.permission')
+            ->where(function ($query) use ($role_id) {
+                $query->where('nav__project.permission_id', null)
+                    ->orWhere(function ($query) use ($role_id) {
+                        $query->selectRaw('count(auth__role_permission.permission_id)')
+                            ->from('auth__role_permission')
+                            ->where('auth__role_permission.permission_id', 'auth__permission.id')
+                            ->where('auth__role_permission.role_id', $role_id)
+                            ->get();
+                    }, ">", 0);
+            })
+            ->where('in_overview', true)
+            ->orderBy('updated_at', 'desc')
+            ->get();
     }
 }
