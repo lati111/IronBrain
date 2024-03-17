27170b60905f1918b72cf21b8df02daddf72f4af
lati111 <75079614+lati111@users.noreply.github.com>
2024-03-17 09:47:27 +0100

    Removed build files

--- public/build/assets/app-1bcbe9b0.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap-f2f7b3fa.js";import"./index-1bf8d157.js";
--- public/build/assets/permission_toggle-bed5dba6.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./ajax-3bdbf024.js";async function e(a,o){const s=new FormData;a===!0?s.append("hasPermission","1"):s.append("hasPermission","0"),await i(o,s)}window.togglePermission=e;
